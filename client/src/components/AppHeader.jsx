504dccf7c4536bfdd82085a9593164aba3aebd20
tong <reangsak1234@hotmail.com>
2024-11-01 13:25:49 +0700

    feat: tong-react context assignment

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,13 @@
+import { userContext } from "../App";
+import { useContext } from "react";
+
 function AppHeader() {
+  const { userData } = useContext(userContext);
+
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {userData.username}</h2>
     </div>
   );
 }
